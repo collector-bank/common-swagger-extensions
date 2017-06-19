3e803305d1eb1cd8b2d00c4599d2df87b233f2ca
Marcus M <marcus.malmestad@collectorbank.se>
2017-06-19 08:51:44 +0200

    Extensions to be able to style swagger the collector way and enable bearer token.

---
+++ src/Collector.Common.Swagger.Extensions/Resources/clientcredentials.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    var domCache = {
+        apiKeyInput: $('#input_apiKey'),
+        exploreButton: $('#explore')
+    };
+
+    function addApiKeyAuthorization() {
+        var key = domCache.apiKeyInput.val();
+
+        if (key && key.trim() != "") {
+            var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("Authorization", "Bearer " + key, "header");
+            window.swaggerUi.api.clientAuthorizations.add("bearer", apiKeyAuth);
+            window.swaggerUi.api.clientAuthorizations.remove("api_key");
+        }
+    }
+
+    domCache.apiKeyInput.change(addApiKeyAuthorization);
+    domCache.apiKeyInput.attr('placeholder', 'Put bearer token here');
+    domCache.exploreButton.hide();
+})();
---
+++ src/Collector.Common.Swagger.Extensions/SwaggerConfigurationExtensions.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SwaggerConfigurationExtensions.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// <summary>
+//   The collector swagger configuration extensions.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Collector.Common.Swagger.Extensions
+{
+    using Swashbuckle.Application;
+    
+    /// <summary>
+    /// The collector swagger configuration extensions.
+    /// </summary>
+    public static class SwaggerConfigurationExtensions
+    {
+        /// <summary>
+        /// Use this to enable the collector style-sheet
+        /// </summary>
+        /// <param name="swaggerUiConfig">
+        /// The swagger User Interface Config
+        /// </param>
+        public static void EnableCollectorTheme(this SwaggerUiConfig swaggerUiConfig)
+        {
+            swaggerUiConfig.InjectStylesheet(typeof(SwaggerConfigurationExtensions).Assembly, "Collector.Common.Swagger.Extensions.Resources.collectortheme.css");
+        }
+
+        /// <summary>
+        /// Use this to enable bearer token in user interface.
+        /// </summary>
+        /// <param name="swaggerUiConfig">
+        /// The swagger User Interface Config
+        /// </param>
+        public static void EnableBearerToken(this SwaggerUiConfig swaggerUiConfig)
+        {
+            swaggerUiConfig.InjectJavaScript(typeof(SwaggerConfigurationExtensions).Assembly, "Collector.Common.Swagger.Extensions.Resources.clientcredentials.js");
+        }
+    }
+
+}
