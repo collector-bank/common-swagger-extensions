a429bc9f5674afa26c73b1719fc42ac082e176be
Mattias Jakobsson <mattias@mattias-jakobsson.net>
2017-09-11 16:14:23 +0200

    Refactored hide check

--- src/Collector.Common.Swagger.Extensions/Security/SwaggerAuthorizationFilter.cs
+++ src/Collector.Common.Swagger.Extensions/Security/SwaggerAuthorizationFilter.cs
@@ -19,20 +19,6 @@ namespace Collector.Common.Swagger.Extensions.Security
         {
             var usedDefinitions = new List<string>();
 
-            IEnumerable<string> GettAllDefinitionsFrom(Operation item)
-            {
-                var response = new List<string>();
-
-                if (item == null)
-                    return response;
-
-                response.AddRange(item.parameters.Where(x => !string.IsNullOrEmpty(x.schema?.@ref)).Select(x => x.schema.@ref.Split('/').LastOrDefault()));
-
-                response.AddRange(item.responses.Where(x => !string.IsNullOrEmpty(x.Value.schema?.@ref)).Select(x => x.Value.schema?.@ref.Split('/').LastOrDefault()));
-
-                return response;
-            }
-
             var descriptions = apiExplorer.ApiDescriptions;
             foreach (var description in descriptions)
             {
@@ -45,40 +31,77 @@ namespace Collector.Common.Swagger.Extensions.Security
                 {
                     switch (description.HttpMethod.Method)
                     {
-                        case "DELETE": usedDefinitions.AddRange(GettAllDefinitionsFrom(path.delete)); break;
-                        case "GET": usedDefinitions.AddRange(GettAllDefinitionsFrom(path.get)); break;
-                        case "HEAD": usedDefinitions.AddRange(GettAllDefinitionsFrom(path.head)); break;
-                        case "OPTIONS": usedDefinitions.AddRange(GettAllDefinitionsFrom(path.options)); break;
-                        case "PATCH": usedDefinitions.AddRange(GettAllDefinitionsFrom(path.patch)); break;
-                        case "POST": usedDefinitions.AddRange(GettAllDefinitionsFrom(path.post)); break;
-                        case "PUT": usedDefinitions.AddRange(GettAllDefinitionsFrom(path.put)); break;
+                        case "DELETE": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.delete)); break;
+                        case "GET": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.get)); break;
+                        case "HEAD": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.head)); break;
+                        case "OPTIONS": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.options)); break;
+                        case "PATCH": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.patch)); break;
+                        case "POST": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.post)); break;
+                        case "PUT": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.put)); break;
                     }
 
                     continue;
                 }
 
-                switch (description.HttpMethod.Method)
-                {
-                    case "DELETE": path.delete = null; break;
-                    case "GET": path.get = null; break;
-                    case "HEAD": path.head = null; break;
-                    case "OPTIONS": path.options = null; break;
-                    case "PATCH": path.patch = null; break;
-                    case "POST": path.post = null; break;
-                    case "PUT": path.put = null; break;
-                    default: throw new ArgumentOutOfRangeException("Method name not mapped to operation");
-                }
-
-                if (path.delete == null && path.get == null &&
-                    path.head == null && path.options == null &&
-                    path.patch == null && path.post == null && path.put == null)
-                    swaggerDoc.paths.Remove(route);
+                HideAction(swaggerDoc, description, path, route);
             }
 
             var removedDefinitions = swaggerDoc.definitions.Keys.Except(usedDefinitions.Distinct()).ToList();
 
             foreach (var removedDefinition in removedDefinitions)
                 swaggerDoc.definitions.Remove(removedDefinition);
+        }
+
+        private static void HideAction(SwaggerDocument swaggerDoc, ApiDescription description, PathItem path, string route)
+        {
+            switch (description.HttpMethod.Method)
+            {
+                case "DELETE":
+                    path.delete = null;
+                    break;
+                case "GET":
+                    path.get = null;
+                    break;
+                case "HEAD":
+                    path.head = null;
+                    break;
+                case "OPTIONS":
+                    path.options = null;
+                    break;
+                case "PATCH":
+                    path.patch = null;
+                    break;
+                case "POST":
+                    path.post = null;
+                    break;
+                case "PUT":
+                    path.put = null;
+                    break;
+                default: throw new ArgumentOutOfRangeException("Method name not mapped to operation");
+            }
+
+            if (path.delete == null && path.get == null &&
+                path.head == null && path.options == null &&
+                path.patch == null && path.post == null && path.put == null)
+                swaggerDoc.paths.Remove(route);
+        }
+
+        private IEnumerable<string> GetAllDefinitionsFrom(Operation item)
+        {
+            var response = new List<string>();
+
+            if (item == null)
+                return response;
+
+            response.AddRange(item.parameters?
+                .Where(x => !string.IsNullOrEmpty(x.schema?.@ref))
+                .Select(x => x.schema.@ref.Split('/').LastOrDefault()) ?? Enumerable.Empty<string>());
+
+            response.AddRange(item.responses?
+                .Where(x => !string.IsNullOrEmpty(x.Value.schema?.@ref))
+                .Select(x => x.Value.schema?.@ref.Split('/').LastOrDefault()) ?? Enumerable.Empty<string>());
+
+            return response;
         }
     }
 }
