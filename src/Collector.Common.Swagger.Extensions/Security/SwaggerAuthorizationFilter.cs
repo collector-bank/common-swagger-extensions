ab8da22355bac0669451e07b60e793ec37f5c7cf
Mattias Jakobsson <mattias@mattias-jakobsson.net>
2017-09-11 15:26:06 +0200

    Hiding definitions when user is unauthorized

--- src/Collector.Common.Swagger.Extensions/Security/SwaggerAuthorizationFilter.cs
+++ src/Collector.Common.Swagger.Extensions/Security/SwaggerAuthorizationFilter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http.Description;
 using Swashbuckle.Swagger;
 
@@ -15,16 +17,42 @@ namespace Collector.Common.Swagger.Extensions.Security
 
         public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
         {
+            var usedDefinitions = new List<string>();
+
+            IEnumerable<string> GettAllDefinitionsFrom(Operation item)
+            {
+                var response = new List<string>();
+
+                if (item == null)
+                    return response;
+
+                response.AddRange(item.parameters.Where(x => !string.IsNullOrEmpty(x.schema?.@ref)).Select(x => x.schema.@ref.Split('/').LastOrDefault()));
+
+                response.AddRange(item.responses.Where(x => !string.IsNullOrEmpty(x.Value.schema?.@ref)).Select(x => x.Value.schema?.@ref.Split('/').LastOrDefault()));
+
+                return response;
+            }
+
             var descriptions = apiExplorer.ApiDescriptions;
             foreach (var description in descriptions)
             {
-                var notShowen = _showAction?.Invoke(description) ?? false;
-
-                if (!notShowen)
-                    continue;
-
                 var route = "/" + description.Route.RouteTemplate.TrimEnd('/');
                 var path = swaggerDoc.paths[route];
+
+                var hideAction = _showAction?.Invoke(description) ?? false;
+
+                if (!hideAction)
+                {
+                    usedDefinitions.AddRange(GettAllDefinitionsFrom(path.delete));
+                    usedDefinitions.AddRange(GettAllDefinitionsFrom(path.get));
+                    usedDefinitions.AddRange(GettAllDefinitionsFrom(path.head));
+                    usedDefinitions.AddRange(GettAllDefinitionsFrom(path.options));
+                    usedDefinitions.AddRange(GettAllDefinitionsFrom(path.patch));
+                    usedDefinitions.AddRange(GettAllDefinitionsFrom(path.post));
+                    usedDefinitions.AddRange(GettAllDefinitionsFrom(path.put));
+
+                    continue;
+                }
 
                 switch (description.HttpMethod.Method)
                 {
@@ -43,6 +71,11 @@ namespace Collector.Common.Swagger.Extensions.Security
                     path.patch == null && path.post == null && path.put == null)
                     swaggerDoc.paths.Remove(route);
             }
+
+            var removedDefinitions = swaggerDoc.definitions.Keys.Except(usedDefinitions.Distinct()).ToList();
+
+            foreach (var removedDefinition in removedDefinitions)
+                swaggerDoc.definitions.Remove(removedDefinition);
         }
     }
 }
