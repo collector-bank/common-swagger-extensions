12c651a29ad1d260358124f07a1f7ed9c3bad122
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2017-09-12 12:51:50 +0200

    Inverse authorization check (#6)
    Inverted autorization when checking if user has access to endpoint
    Removing definitions when unauthorized for DotNetCore-package

--- src/Collector.Common.Swagger.AspNetCore.Extensions/Security/SwaggerAuthorizationFilter.cs
+++ src/Collector.Common.Swagger.AspNetCore.Extensions/Security/SwaggerAuthorizationFilter.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -27,6 +28,8 @@ namespace Collector.Common.Swagger.AspNetCore.Extensions.Security
         {
             var http = this._provider.GetRequiredService<IHttpContextAccessor>();
             var auth = this._provider.GetRequiredService<IAuthorizationService>();
+            
+            var usedDefinitions = new List<string>();
 
             var descriptions = context.ApiDescriptionsGroups.Items.SelectMany(group => group.Items);
 
@@ -38,52 +41,107 @@ namespace Collector.Common.Swagger.AspNetCore.Extensions.Security
                         .OfType<AuthorizeAttribute>());
 
                 // check if this action should be visible
-                var notShowen = IsForbiddenDueAnonymous(http, authAttributes) ||
-                                IsForbiddenDuePolicy(http, auth, authAttributes);
-
-                if (!notShowen)
-                    continue; // user passed all permissions checks
-
+                var showCurrentAction = IsUserAllowed(http, authAttributes) &&
+                                IsPolicyAllowed(http, auth, authAttributes);
+                
                 var route = "/" + description.RelativePath.TrimEnd('/');
                 var path = swaggerDoc.Paths[route];
 
-                // remove method or entire path (if there are no more methods in this path)
-                switch (description.HttpMethod)
+                if (showCurrentAction)
                 {
-                    case "DELETE": path.Delete = null; break;
-                    case "GET": path.Get = null; break;
-                    case "HEAD": path.Head = null; break;
-                    case "OPTIONS": path.Options = null; break;
-                    case "PATCH": path.Patch = null; break;
-                    case "POST": path.Post = null; break;
-                    case "PUT": path.Put = null; break;
-                    default: throw new ArgumentOutOfRangeException("Method name not mapped to operation");
+                    switch (description.HttpMethod)
+                    {
+                        case "DELETE": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.Delete)); break;
+                        case "GET": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.Get)); break;
+                        case "HEAD": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.Head)); break;
+                        case "OPTIONS": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.Options)); break;
+                        case "PATCH": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.Patch)); break;
+                        case "POST": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.Post)); break;
+                        case "PUT": usedDefinitions.AddRange(GetAllDefinitionsFrom(path.Put)); break;
+                    }
+
+                    continue;
                 }
 
-                if (path.Delete == null && path.Get == null &&
-                    path.Head == null && path.Options == null &&
-                    path.Patch == null && path.Post == null && path.Put == null)
-                    swaggerDoc.Paths.Remove(route);
+                HideAction(swaggerDoc, description, path, route);
             }
+            
+            var removedDefinitions = swaggerDoc.Definitions.Keys.Except(usedDefinitions.Distinct()).ToList();
+
+            foreach (var removedDefinition in removedDefinitions)
+                swaggerDoc.Definitions.Remove(removedDefinition);
         }
 
-        private static bool IsForbiddenDuePolicy(
+        private static void HideAction(SwaggerDocument swaggerDoc, ApiDescription description, PathItem path, string route)
+        {
+            // remove method or entire path (if there are no more methods in this path)
+            switch (description.HttpMethod)
+            {
+                case "DELETE":
+                    path.Delete = null;
+                    break;
+                case "GET":
+                    path.Get = null;
+                    break;
+                case "HEAD":
+                    path.Head = null;
+                    break;
+                case "OPTIONS":
+                    path.Options = null;
+                    break;
+                case "PATCH":
+                    path.Patch = null;
+                    break;
+                case "POST":
+                    path.Post = null;
+                    break;
+                case "PUT":
+                    path.Put = null;
+                    break;
+                default: throw new ArgumentOutOfRangeException("Method name not mapped to operation");
+            }
+
+            if (path.Delete == null && path.Get == null &&
+                path.Head == null && path.Options == null &&
+                path.Patch == null && path.Post == null && path.Put == null)
+                swaggerDoc.Paths.Remove(route);
+        }
+
+        private static IEnumerable<string> GetAllDefinitionsFrom(Operation item)
+        {
+            var response = new List<string>();
+
+            if (item == null)
+                return response;
+            
+            response.AddRange(item.Schemes?
+                                  .Select(x => x.Split('/').LastOrDefault()) ?? Enumerable.Empty<string>());
+
+            response.AddRange(item.Responses?
+                                  .Where(x => !string.IsNullOrEmpty(x.Value.Schema?.Ref))
+                                  .Select(x => x.Value.Schema?.Ref.Split('/').LastOrDefault()) ?? Enumerable.Empty<string>());
+
+            return response;
+        }
+
+        private static bool IsPolicyAllowed(
             IHttpContextAccessor http,
             IAuthorizationService auth,
             IEnumerable<AuthorizeAttribute> attributes)
         {
             var policies = attributes
-                .Where(p => !String.IsNullOrEmpty(p.Policy))
+                .Where(p => !string.IsNullOrEmpty(p.Policy))
                 .Select(a => a.Policy)
                 .Distinct();
-            return policies.Any(p => Task.Run(async () => await auth.AuthorizeAsync(http.HttpContext.User, p)).Result == false);
+            
+            return policies.All(p => Task.Run(async () => await auth.AuthorizeAsync(http.HttpContext.User, p)).Result);
         }
 
-        private static bool IsForbiddenDueAnonymous(
+        private static bool IsUserAllowed(
             IHttpContextAccessor http,
             IEnumerable<AuthorizeAttribute> attributes)
         {
-            return attributes.Any() && !http.HttpContext.User.Identity.IsAuthenticated;
+            return !attributes.Any() || http.HttpContext.User.Identity.IsAuthenticated;
         }
     }
 }
